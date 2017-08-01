6be354c0e1facb9202d3244238c1d5e352828aab
new <new>
2017-08-01 15:34:19 +0800

    IOT init

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,9 @@
 <?php
 	   /*
-	   con_db_host = "ttggcc.dot.vip"
-	   con_db_id   = "ttggcc"
-	   con_db_pass	= "xjwl6677123"
-	   con_db_name = "ttggcc"
+	   con_db_host = "localhost"
+	   con_db_id   = "root"
+	   con_db_pass	= ""
+	   con_db_name = "xjtuiot"
 	   tablepre    =  "met_"
 	   db_charset  =  "utf8";
 	  */
